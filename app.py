79a4054031870442ca79a53cdeb2c7f658a9551b
Krishna Therokar <krishnatherokar@gmail.com>
2026-02-11 16:57:24 +0530

    add example images

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import gradio as gr
 from utils import predict_and_decode
+import os
+
+example_path = "examples"
+example_list = [os.path.join(example_path, f) for f in os.listdir(example_path)]
 
 interface = gr.Interface(
     fn=predict_and_decode,
     inputs=gr.Image(type="pil"),
     outputs="text",
-    title="Captcha Decoder"
+    title="Captcha Decoder",
+    examples=example_list
 )
 
 interface.launch()
